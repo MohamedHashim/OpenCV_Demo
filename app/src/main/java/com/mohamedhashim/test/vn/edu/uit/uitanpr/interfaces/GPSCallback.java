50a06adc64d7aa948369c08f2ca83d4efe0cef7f
MohamedHashim <sawmido2000@gmail.com>
2017-07-08 05:56:51 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedhashim/test/vn/edu/uit/uitanpr/interfaces/GPSCallback.java
@@ -0,0 +1,7 @@
+package com.mohamedhashim.test.vn.edu.uit.uitanpr.interfaces;
+
+import android.location.Location;
+
+public interface GPSCallback {
+	public abstract void onGPSUpdate(Location location);
+}
---
+++ app/src/main/java/com/mohamedhashim/test/vn/edu/uit/uitanpr/interfaces/OnTaskCompleted.java
@@ -0,0 +1,5 @@
+package com.mohamedhashim.test.vn.edu.uit.uitanpr.interfaces;
+
+public interface OnTaskCompleted {
+	void updateResult(String result);
+}
